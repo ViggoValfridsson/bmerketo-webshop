6c57299b108f964129d66243034456cc9a22495b
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-12 13:55:08 +0200

    Created home page viewmodel

---
+++ bmerketo-webshop/Views/ViewModel/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bmerketo_webshop.Views.ViewModel;
+
+public class HomeViewModel
+{
+    [Required]
+    [MinLength(6)]
+    [MaxLength(320)]
+    [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")]
+    public string Email { get; set; } = null!;
+}
