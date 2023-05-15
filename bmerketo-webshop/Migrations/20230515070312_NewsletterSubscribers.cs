dd69c2e50e4033ee71847ff34bb5e67d3a69b0ce
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-15 09:03:48 +0200

    Created newsletter entity

---
+++ bmerketo-webshop/Migrations/20230515070312_NewsletterSubscribers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bmerketo_webshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewsletterSubscribers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NewsletterSubscribers",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(320)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsletterSubscribers", x => x.Email);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NewsletterSubscribers");
+        }
+    }
+}
