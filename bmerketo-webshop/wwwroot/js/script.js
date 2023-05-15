8f0fe1bf08f70b06dddec7f19ab0858d8f8de521
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-15 10:22:49 +0200

    Added aria information to toggle menu

--- bmerketo-webshop/wwwroot/js/script.js
+++ bmerketo-webshop/wwwroot/js/script.js
@@ -1,12 +1,1 @@
-﻿const toggleMobileMenu = () => {
-    try {
-        const menu = document.querySelector("#menu")
-        const openIcon = document.querySelector(".open-icon")
-        const closeIcon = document.querySelector(".close-icon")
-
-        menu.classList.toggle("mobile-closed-menu");
-        openIcon.classList.toggle("hidden");
-        closeIcon.classList.toggle("hidden");
-    }
-    catch { }
-}
+﻿
