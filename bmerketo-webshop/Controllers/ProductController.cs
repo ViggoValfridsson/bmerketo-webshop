54014c2a58f49b8721e001239f326031e4654990
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-17 11:08:35 +0200

    Created layout for product item page

---
+++ bmerketo-webshop/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using bmerketo_webshop.Helpers.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bmerketo_webshop.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly ProductService _productService;
+
+    public ProductController(ProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [Route("product/{articleId}")]
+    public async Task<IActionResult> Index(string articleId)
+    {
+        var product = await _productService.GetAsync(x => x.ArticleId == articleId);
+
+        if (product == null)
+            return NotFound();
+
+        return View(product);
+    }
+}
