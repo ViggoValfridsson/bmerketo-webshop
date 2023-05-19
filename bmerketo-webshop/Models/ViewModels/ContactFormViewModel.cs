a9272f42ae85ed52fbca24eb3b143aa6b5d7ab0e
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-19 11:35:06 +0200

    Styled contact page

--- bmerketo-webshop/Models/ViewModels/ContactFormViewModel.cs
+++ bmerketo-webshop/Models/ViewModels/ContactFormViewModel.cs
@@ -1,25 +1,33 @@
-﻿using bmerketo_webshop.Migrations;
-using bmerketo_webshop.Models.Entities;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace bmerketo_webshop.Models.ViewModels;
 
 public class ContactFormViewModel
 {
+    [Required(ErrorMessage = "You must provide a first name.")]
+    [MaxLength(100, ErrorMessage = "The maximum length is 100 characters.")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "You must provide a last name.")]
+    [MaxLength(320, ErrorMessage = "The maximum length is 100 characters.")]
+    public string LastName { get; set; } = null!;
+
     [Required(ErrorMessage = "You must provide an email address.")]
     [MinLength(6, ErrorMessage = "Invalid email. Enter a valid email and try again.")]
     [MaxLength(320, ErrorMessage = "Email is too long. Please enter less than 320 characters.")]
     [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email. Enter a valid email and try again.")]
     public string Email { get; set; } = null!;
 
-    public static implicit operator NewsletterSubscriberEntity(ContactFormViewModel model)
-    {
-        if (model == null)
-            return null!;
+    [Required(ErrorMessage = "You must provide a phone number.")]
+    [MaxLength(15, ErrorMessage = "Please enter less than 15 characters.")]
+    [RegularExpression(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$", ErrorMessage = "Not a valid phone number. Please try again")]
+    public string PhoneNumber { get; set; } = null!;
 
-        return new NewsletterSubscriberEntity
-        {
-            Email = model.Email
-        };
-    }
+    [MaxLength(100, ErrorMessage = "The maximum length is 100 characters.")]
+    public string? CompanyName { get; set; }
+
+    [MaxLength(4000, ErrorMessage = "Message is too long. Please try again with less than 4000 characters.")]
+    public string Content { get; set; } = null!;
 }
