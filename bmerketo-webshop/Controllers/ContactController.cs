0874cc1d11c0ca3087c6e27f5d7f335df30ad8ef
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-19 14:01:02 +0200

    Updated success message

--- bmerketo-webshop/Controllers/ContactController.cs
+++ bmerketo-webshop/Controllers/ContactController.cs
@@ -21,6 +21,8 @@ public class ContactController : Controller
     [HttpPost]
     public async Task<IActionResult> Index(ContactFormViewModel model)
     {
+        TempData["SuccessMessage"] = null;
+
         if (ModelState.IsValid)
         {
             if (await _contactFormRepo.CreateAsync(model) != null)
