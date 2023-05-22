7573072c2a90c2607a8df3dac7d49ed9e5ca335e
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-22 09:13:32 +0200

    Changed modal name to be more generic

---
+++ bmerketo-webshop/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bmerketo_webshop.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
