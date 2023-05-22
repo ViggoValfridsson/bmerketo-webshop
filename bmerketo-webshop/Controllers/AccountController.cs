d1d8c2074f11fe7b2cf4627ca4e475dedf2ff370
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-22 11:15:02 +0200

    Styled log in page

--- bmerketo-webshop/Controllers/AccountController.cs
+++ bmerketo-webshop/Controllers/AccountController.cs
@@ -1,10 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using bmerketo_webshop.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace bmerketo_webshop.Controllers;
 
 public class AccountController : Controller
 {
     public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult LogIn()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult LogIn(LogInViewModel viewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            ModelState.AddModelError("", "Not implemented");
+        }
+
+        return View(viewModel);
+    }
+
+    public IActionResult SignUp()
     {
         return View();
     }
