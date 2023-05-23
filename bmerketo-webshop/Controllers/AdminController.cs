b1ec3667ed92a974ff4dcfdf05085381ce7f3fd8
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-23 13:11:01 +0200

    added header link to admin dashboard

---
+++ bmerketo-webshop/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bmerketo_webshop.Controllers;
+
+public class AdminController : Controller
+{
+    [Authorize(Roles = "admin")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ bmerketo-webshop/Controllers/DeniedController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bmerketo_webshop.Controllers;
+
+public class DeniedController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
